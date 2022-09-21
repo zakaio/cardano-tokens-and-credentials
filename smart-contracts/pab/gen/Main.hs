c3df2e1417e770fa6c0170f66176b82e85ec139f
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2022-09-21 14:28:22 +0300

    implements configuration and output to stdout

--- smart-contracts/pab/gen/Main.hs
+++ smart-contracts/pab/gen/Main.hs
@@ -18,47 +18,70 @@ module Main(main) where
 import           Cardano.Api
 import           Cardano.Api.Shelley                 (PlutusScript (..))
 import           Codec.Serialise                     (serialise)
-import           Data.Aeson                          (FromJSON (..), ToJSON (..))
+import           Data.Aeson.Encode.Pretty            (defConfig, encodePretty' )
 import qualified Data.ByteString.Lazy  as LBS
 import qualified Data.ByteString.Short as SBS
 import           Data.Time
-import           Data.Time.Clock
 import           Data.Time.Clock.POSIX               (getPOSIXTime)
-import           PlutusTx
-import qualified PlutusTx
 import qualified Ledger
 import qualified Ledger.Scripts                       as Scripts
 import qualified Plutus.Script.Utils.V1.Typed.Scripts as Scripts
 import qualified System.Environment
+import qualified System.Exit
 
---import qualified Ledger.Typed.Scripts as Scripts
---import qualified PlutusLedgerApi.V1.Scripts as Scripts
 import           ProofSpace.Contracts.OnChain.LockValue  
 
+data Options = Options {
+    outFile :: String,
+    stdout :: Bool
+}
 
-help :: IO (Either String String)
+
+
+help :: IO (Either String a)
 help = do
-            _ <- print("Usage: proofspace-token-credential-gen script params")
-            _ <- print("where script is one-of")
+            _ <- print("Usage: proofspace-token-credential-gen [options] script params")
+            _ <- print("options is one of:")
+            _ <- print("   --out fname     output script to fname (default: onchain.json)")
+            _ <- print("   --stdout        output script to stdout")
+            _ <- print("")
+            _ <- print("and script params is one-of:")
             _ <- print("   lock-value  [posix-time]")
             _ <- print("                           ")
             return (Left "No operation to perform" ) 
 
-writeValidator :: FilePath -> Scripts.Validator -> IO (Either (FileError ()) ())
-writeValidator file = writeFileTextEnvelope @(PlutusScript PlutusScriptV1) file Nothing . PlutusScriptSerialised . SBS.toShort . LBS.toStrict . serialise . Scripts.unValidatorScript
+writeValidator :: Options -> Scripts.Validator -> IO (Either (FileError ()) (Maybe String))
+writeValidator options validator = do
+    let serializedScript = PlutusScriptSerialised . SBS.toShort . LBS.toStrict . serialise . Scripts.unValidatorScript
+    if (stdout options) then do
+        -- not exported form cardano-node
+        --let content = textEnvelopeToJSON @(PlutusScript PlutusScriptV1) Nothing (serializedScript validator)
+        let content = encodePretty'  defConfig (serialiseToTextEnvelope @(PlutusScript PlutusScriptV1)  Nothing (serializedScript validator))
+        _ <- print(content)
+        return (Right Nothing) 
+    else
+        do
+            let file = outFile options
+            writeResult <- writeFileTextEnvelope @(PlutusScript PlutusScriptV1) file Nothing (serializedScript validator)
+            let result = case writeResult of
+                    Left err -> Left err
+                    Right _  -> Right (Just file) 
+            return result
+        
 
-writeLockValueValidaror :: Ledger.POSIXTime -> FilePath -> IO (Either (FileError ()) ())
-writeLockValueValidaror expireTime fname = do
+
+writeLockValueValidaror :: Ledger.POSIXTime -> Options -> IO (Either (FileError ()) (Maybe String))
+writeLockValueValidaror expireTime options = do
     let scriptParams = LockValueParams {
         lvpExpireTime = expireTime
     }
     let validatorInstance = validateLockValueInstance scriptParams
     let script = Scripts.validatorScript validatorInstance
-    writeResult <- writeValidator fname script
+    writeResult <- writeValidator options script
     return writeResult
 
-genLockValue :: [String] -> IO (Either String String)
-genLockValue nextArgs =
+genLockValue :: [String] -> Options -> IO (Either String (Maybe String))
+genLockValue nextArgs options =
     if null nextArgs then
         return $ Left "lock-value require argument (epoch)"
     else 
@@ -71,36 +94,51 @@ genLockValue nextArgs =
                 return  $ Left "Expire time is on the past"
             else
                 do
-                    let fname = "validator.dat"
-                    writeResult <- writeLockValueValidaror (Ledger.POSIXTime seconds) "validator" 
+                    writeResult <- writeLockValueValidaror (Ledger.POSIXTime seconds) options
                     let result = case writeResult of
                                     Left err  ->  Left (show err)
-                                    Right _   ->  Right fname
+                                    Right optFname   ->  Right optFname
                     return result
-        
 
-        
-    
+              
 
+command :: [String] -> Options  -> IO (Either String (Maybe String))
+command args options =  
+            if null args then 
+                help
+            else
+                case head args of
+                    "--out"  -> commandWithOutOption (tail args) options
+                    "--stdout" -> command (tail args) (options {stdout = True})
+                    "lock-value" -> genLockValue (tail args) options
+                    other  ->
+                            return (Left ("Unknown script: "<> other))
+
+commandWithOutOption :: [String] -> Options -> IO (Either String (Maybe String))
+commandWithOutOption args options = 
+    if null args then
+        return (Left "--out option miss fname")
+    else
+        command (tail args) (options { outFile = (head args) })
+        
 
 
 main :: IO ()
 main = do
             args <- System.Environment.getArgs
-            result <- if null args then 
-                        help
-                    else
-                        case head args of
-                            "lock-value" -> genLockValue (tail args)
-                            other  ->
-                                return (Left ("Unknown script: "<> other))            
+            let emptyOptions = Options {
+                outFile = "onchain.json",
+                stdout = False
+            }
+            result <- command args emptyOptions
             case result of
                 Left message -> do
-                                   _ <- print("No operation performed:" <> message)
+                                   _ <- System.Exit.die("No operation performed:" <> message)
                                    return ()
-                Right fname  -> do 
-                                   print("ok:"<>fname)
-                                   return ()
+                Right (Just fname)  -> do 
+                                     print("ok:"<> fname)
+                                     return ()
+                Right Nothing -> return ()
                                    
 
         
